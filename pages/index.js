44827473bc0cc93c5fbac2b754fb670c9cc146ba
Rachel Ombok <43306992+rachelombok@users.noreply.github.com>
2023-01-08 12:53:54 -0500

    updating tagline

--- pages/index.js
+++ pages/index.js
@@ -100,7 +100,7 @@ const Home = () => {
             <h1>Rachel's AI Avatar Generator</h1>
           </div>
           <div className="header-subtitle">
-            <h2>Turn me into anyone you want! Make sure you refer to me as "abraza" in the prompt</h2>
+            <h2>Turn me into anyone you want! Make sure you refer to me as "rachelombok" in the prompt</h2>
           </div>
           <div className="prompt-container">
         <input className="prompt-box" value={input} onChange={onChange}/>
